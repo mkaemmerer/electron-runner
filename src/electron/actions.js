0e045d760f66dcf5f9cc492d5ec15bfcada2e838
Matt Kaemmerer <matthew.kaemmerer@gmail.com>
2016-10-25 18:56:49 -0500

    Refactor wait argument order

--- src/electron/actions.js
+++ src/electron/actions.js
@@ -47,31 +47,25 @@ export function type(done, selector, text) {
  * @param {...} args
  */
 
-export function wait(done, ...args){
-  if (args.length === 0) {
+export function wait(done, condition, ...args){
+  if (condition === undefined) {
     return done();
   }
 
-  let arg = args[0];
-  if (typeof arg === 'number') {
-    if(arg < this.options.waitTimeout){
-      waitms(arg, done);
+  if (typeof condition === 'number') {
+    if(condition < this.options.waitTimeout){
+      waitms(this, done, condition);
     } else {
-      waitms(this.options.waitTimeout, () => {
+      waitms(this, () => {
         done(new Error('.wait() timed out after '+this.options.waitTimeout+'msec'));
-      });
+      }, this.options.waitTimeout);
     }
   }
-  else if (typeof arg === 'string') {
-    let timeout = null; 
-    if (typeof args[1] === 'number') { 
-      timeout = args[1];
-    } 
-    waitelem.apply({ timeout: timeout }, [this, arg, done]);
+  else if (typeof condition === 'string') {
+    waitelem(this, done, condition);
   }
-  else if (typeof arg === 'function') {
-    args.unshift(this);
-    waitfn.apply(this, args);
+  else if (typeof condition === 'function') {
+    waitfn(this, done, condition, ...args);
   }
   else {
     done();
@@ -81,11 +75,12 @@ export function wait(done, ...args){
 /**
  * Wait for a specififed amount of time.
  *
- * @param {Number} ms
+ * @param {Driver} self
  * @param {Function} done
+ * @param {Number} ms
  */
 
-function waitms (ms, done) {
+function waitms (self, done, ms) {
   setTimeout(done, ms);
 }
 
@@ -93,55 +88,46 @@ function waitms (ms, done) {
  * Wait for a specified selector to exist.
  *
  * @param {Driver} self
- * @param {String} selector
  * @param {Function} done
+ * @param {String} selector
  */
 
-function waitelem (self, selector, done) {
-  let elementPresent = new Function(`
-    'use strict';
-    let element = document.querySelector('${JSON.stringify(selector).slice(1,-1)}');
-    return (element ? true : false);
-  `);
-  waitfn.apply(this, [self, elementPresent, done]);
+function waitelem (self, done, selector) {
+  let elementPresent = function(selector){
+    return (document.querySelector(selector) ? true : false);
+  };
+  waitfn(self, done, elementPresent, selector);
 }
 
 /**
  * Wait until evaluated function returns true.
  *
  * @param {Driver} self
+ * @param {Function} done
  * @param {Function} fn
  * @param {...} args
- * @param {Function} done
  */
 
-function waitfn(...args) { 
-  let timeout = this.timeout || null;
+function waitfn(self, done, fn, ...args) { 
   let waitMsPassed = 0;
-  return tick(...args);
+  return tick(self, done, fn, ...args);
 
-  function tick (...args /** self, fn, arg1, arg2..., done**/) {
-    let [self, fn] = args;
-    let done = args[args.length-1];
+  function tick (self, done, fn, ...args) {
     let waitDone = function (err, result) {
       if (result) {
-        return done();
-      }
-      else if (timeout && waitMsPassed > timeout) {
-        return done();
+        done();
       }
       else if (self.options.waitTimeout && waitMsPassed > self.options.waitTimeout) {
-        return done(new Error('.wait() timed out after '+self.options.waitTimeout+'msec'));
+        done(new Error('.wait() timed out after '+self.options.waitTimeout+'msec'));
       }
       else {
         waitMsPassed += self.options.pollInterval;
         setTimeout(function () {
-          tick.apply(self, args);
+          tick(self, done, fn, ...args);
         }, self.options.pollInterval);
       }
     };
-    let newArgs = [waitDone, fn].concat(args.slice(2,-1));
-    self.evaluate_now.apply(self, newArgs);
+    self.evaluate_now(waitDone, fn, ...args);
   }
 }
 
